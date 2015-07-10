0046bb19534d091606b3e5f1f4d8c4525e3475a9
Yun Hyeon-Ki <yhk1030@khu.ac.kr>
2015-07-10 16:22:44 +0900

    re make the R program

--- Kocom/R_Pkgs/installpkgs2.R
+++ Kocom/R_Pkgs/installpkgs2.R
@@ -1,5 +1,5 @@
 cat("==========================================================================")
-install.packages("RSclient_0.7-2.tar.gz",  repos=NULL, type="source")
+install.packages("RSclient_0.7-2.tar.gz", repos=NULL, type="source")
 cat("==========================================================================")
 install.packages("CEMS_0.2.tar.gz", repos=NULL, type="source")
 cat("==================     R Packages install complete.     ==================")
